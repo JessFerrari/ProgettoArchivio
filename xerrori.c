948bc563f55556804e3a81081b537a32b9a90399
FerrariJessica <j.ferrari1@studenti.unipi.it>
2023-09-01 18:09:47 +0200

    xerrori.c

--- xerrori.c
+++ xerrori.c
@@ -4,13 +4,6 @@
 // i prototipi sono in xerrori.h
 
 
-// termina un processo con eventuale messaggio d'errore
-void termina(const char *messaggio) {
-  if(errno==0)  fprintf(stderr,"== %d == %s\n",getpid(), messaggio);
-  else fprintf(stderr,"== %d == %s: %s\n",getpid(), messaggio,
-              strerror(errno));
-  exit(1);
-}
 
 // termina un processo con eventuale messaggio d'errore + linea e file
 void xtermina(const char *messaggio, int linea, char *file) {
